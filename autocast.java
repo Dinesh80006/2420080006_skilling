1a1ce6371bc967ffd541506da64e0e9f892a5321
Dinesh80006 <2420080006@klh.edu.in>
2025-08-05 10:59:40 +0530

    Add files via upload

---
+++ autocast.java
@@ -0,0 +1,12 @@
+package myproject;
+
+public class autocast {
+	public static void main(String args[]){
+		int mylnt = 9;
+		double myDouble = mylnt;
+		
+		System.out.println(myDouble);
+		System.out.println(mylnt);
+	}
+
+}
---
+++ manulcast.java
@@ -0,0 +1,13 @@
+package myproject;
+
+public class manulcast {
+	public static void main(String args[]){
+		double myDouble = 9.78;
+		int mylnt = (int)myDouble;
+			
+			System.out.println(myDouble);
+		System.out.println(mylnt);
+		
+	}
+
+}
